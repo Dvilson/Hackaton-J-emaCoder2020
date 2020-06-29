e993aa184654847eedead1cd584902c44659aec6
Dvilson <papiscamara2@gmail.com>
2020-06-29 22:37:27 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Hackaton-J-emaCoder2020
---
+++ js/main.js
@@ -0,0 +1,30 @@
+const responsive = {
+    0: {
+        items: 1
+    },
+    320: {
+        items: 1
+    },
+    560: {
+        items: 2
+    },
+    960: {
+        items: 3
+    }
+}
+
+$(document).ready(function () {
+
+
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        dots: true,
+        nav: false,
+        responsive: responsive
+    });
+
+true
+ 
+});
